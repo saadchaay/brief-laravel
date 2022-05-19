30f89e61b954b5602ffd1b9814576cb2483434e0
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-19 09:49:28 +0000

    add store method in comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Post;
 
 class CommentController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required',
+        ]);
+
+        $post = Post::find($request->post_id);
+        $post->comments()->create([
+            'body' => $request->body,
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return back();
+    }
 }
