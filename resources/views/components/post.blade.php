06fa80c96e25726a9192101ac912aa13a1e3513b
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-19 15:49:10 +0000

    update post component

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -34,30 +34,42 @@
         <p>
             {{ $post->body }}
         </p>
-        <div class="w-full h-8 flex items-center px-3 my-3">
-            <div class="bg-blue-500 z-10 w-5 h-5 rounded-full flex items-center justify-center ">
-                <svg class="w-3 h-3 fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="#b0b0b0" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>
+        <div class="w-full h-8 flex justify-between items-center px-3 my-3">
+            <div class="flex justify-around">
+                <div class="flex items-center">
+                    
+                    <div class="z-10 rounded-full flex items-center justify-center ">
+                        <svg class="h-6 w-6 text-indigo-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polyline points="23 6 13.5 15.5 8.5 10.5 1 18" />  <polyline points="17 6 23 6 23 12" /></svg>    
+                    </div>
+                    <p class="mx-3 text-gray-500">7 Likes</p>
+                </div>
+                <div class="flex items-center">
+                    <div class="z-10 rounded-full flex items-center justify-center ">
+                        <svg class="h-6 w-6 text-gray-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polyline points="23 18 13.5 8.5 8.5 13.5 1 6" />  <polyline points="17 18 23 18 23 12" /></svg>    
+                    </div>
+                    <p class="mx-3 text-gray-500">7 Dislikes</p>
+                </div>
             </div>
-            <div class="bg-red-500 w-5 h-5 rounded-full flex items-center justify-center -ml-1">
-                <svg  class="w-3 h-3 fill-current stroke-current text-white" xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="#b0b0b0" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
-            </div>
-        
-            <div class="w-full flex justify-between">
-                {{-- update likes count --}}
-                <p class="ml-3 text-gray-500">{{ $post->user->count() }}</p>
-                <p class="ml-3 text-gray-500">29 comment</p>
+            <div class="">
+                @if($comments->count() > 0)
+                    <p class="ml-3 text-gray-500">{{ $comments->count() }} {{ Str::plural('Comment', $comments->count()) }}</p>
+                @endif
             </div>
         </div>
         <hr>
-            <div class="grid grid-cols-3 w-full px-5 px-5 my-3">
-                <button class="flex flex-row justify-center items-center w-full space-x-3"><svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="#838383" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>
-                    <span class="font-semibold text-lg text-gray-600">{{$post->user->count()}} {{ Str::plural('Like', 1) }}</span>
+            <div class="flex justify-between w-full my-3">
+                <button class="flex flex-row justify-center items-center w-full space-x-3">
+                    <span class="font-semibold text-lg text-gray-600">{{$post->user->count()}}
+                        <span class="underline">{{ Str::plural('Like', 1) }}</span> 
+                    </span>
+                </button>
+                <button class="flex flex-row justify-center items-center w-full space-x-3">
+                    <span class="font-semibold text-lg text-gray-600">{{$post->user->count()}}
+                        <span class="underline">{{ Str::plural('Unlike', 1) }}</span> 
+                    </span>
                 </button>
                 <button class="flex flex-row justify-center items-center w-full space-x-3"><svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="#838383" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
-                    <span class="font-semibold text-lg text-gray-600">{{$post->user->count()}} {{ Str::plural('Comment', 2) }}</span>
-                </button>
-                <button class="flex flex-row justify-center items-center w-full space-x-3"><svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="#838383" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg>
-                    <span class="font-semibold text-lg text-gray-600">{{$post->user->count()}} {{ Str::plural('Unlike', 3) }}</span>
+                    <span class="font-semibold text-lg text-gray-600">{{ $comments->count() }} {{ Str::plural('Comment', $comments->count()) }}</span>
                 </button>
             </div>
         <hr>
