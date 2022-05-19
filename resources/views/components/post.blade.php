abdd7fffd72dbba2d372b48976fee6e39f67a023
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-19 23:15:25 +0000

    add like icons

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -39,13 +39,17 @@
                 <div class="flex items-center">
                     
                     <div class="z-10 rounded-full flex items-center justify-center ">
-                        <svg class="h-6 w-6 text-indigo-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polyline points="23 6 13.5 15.5 8.5 10.5 1 18" />  <polyline points="17 6 23 6 23 12" /></svg>    
+                        <svg class="h-6 w-6 text-indigo-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"/>
+                          </svg>                              
                     </div>
                     <p class="mx-3 text-gray-500">7 Likes</p>
                 </div>
                 <div class="flex items-center">
                     <div class="z-10 rounded-full flex items-center justify-center ">
-                        <svg class="h-6 w-6 text-gray-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polyline points="23 18 13.5 8.5 8.5 13.5 1 6" />  <polyline points="17 18 23 18 23 12" /></svg>    
+                        <svg class="h-6 w-6 text-indigo-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5"/>
+                        </svg>    
                     </div>
                     <p class="mx-3 text-gray-500">7 Unlike</p>
                 </div>
@@ -58,7 +62,7 @@
         </div>
         <hr>
             <div class="flex justify-between w-full my-3">
-                <form action="{{ route('likes.store', $post) }}" method="post" class="flex flex-row justify-center items-center">
+                <form action="{{ route('likes.store', $post) }}" method="post" class="flex">
                     @csrf
                     <button class="flex flex-row justify-center items-center w-full space-x-3">
                         <span class="font-semibold text-lg text-gray-600"> {{$post->likes->count()}}
