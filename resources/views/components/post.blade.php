99840cf818835556de0dee3bd88f64b979a4a77f
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-19 23:23:04 +0000

    Update

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -43,7 +43,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"/>
                           </svg>                              
                     </div>
-                    <p class="mx-3 text-gray-500">7 Likes</p>
+                    <p class="mx-3 text-gray-500">{{ $post->likes->count() }} {{ Str::plural('Like', $post->likes->count()) }}</p>
                 </div>
                 <div class="flex items-center">
                     <div class="z-10 rounded-full flex items-center justify-center ">
@@ -62,15 +62,16 @@
         </div>
         <hr>
             <div class="flex justify-between w-full my-3">
-                <form action="{{ route('likes.store', $post) }}" method="post" class="flex">
+                {{-- @if (!$post->likedBy(auth()->user())) --}}
+                <form action="{{ route('likes.store', $post) }}" method="post" class="flex flex-row justify-center items-center w-full">
                     @csrf
-                    <button class="flex flex-row justify-center items-center w-full space-x-3">
-                        <span class="font-semibold text-lg text-gray-600"> {{$post->likes->count()}}
+                    <button type="submit" class="">
+                        <span class="font-semibold text-sm text-gray-600"> {{$post->likes->count()}}
                             <span class="underline">{{ Str::plural('Like', 1) }}</span> 
                         </span>
                     </button>
                 </form>
-                
+                {{-- @endif --}}
                 <button class="flex flex-row justify-center items-center w-full space-x-3">
                     <span class="font-semibold text-lg text-gray-600">{{$post->user->count()}}
                         <span class="underline">{{ Str::plural('Unlike', 1) }}</span> 
