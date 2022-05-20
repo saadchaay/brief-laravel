c38ad125b131b22a15ed426e95a2f7ef6817155f
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-20 10:04:57 +0000

    add form update

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -2,8 +2,59 @@
 
 @section('content')
 <div class="flex flex-col justify-center items-center">
-    <div class="w-8/12 bg-white p-6 rounded-lg font-bold">
+    <div class="w-8/12 bg-white p-6 rounded-lg font-bold mb-10">
         User Profile
     </div>
+    {{-- USER FORM --}}
+    <div class="w-8/12 bg-white p-6 rounded-lg font-bold">
+        <form action="{{ route('profile') }}" method="post">
+            @csrf
+            <div class="flex justify-around flex-wrap ">
+                <div class="w-96 my-2">
+                    <label for="email" class="sr-only">Email</label>
+                    <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+
+                    @error('email')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div class="w-96 my-2">
+                    <label for="email" class="sr-only">Email</label>
+                    <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+
+                    @error('email')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+            <div class="flex justify-around flex-wrap ">
+                <div class="w-96 my-2">
+                    <label for="email" class="sr-only">Email</label>
+                    <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+
+                    @error('email')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div class="w-96 my-2">
+                    <label for="email" class="sr-only">Email</label>
+                    <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+
+                    @error('email')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
+
 @endsection
