5b8be5879697f1454ea4d3b6f50306d30e40ae92
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-20 10:52:56 +0000

    add phone in user table

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+    
     public function index()
     {
-        return view('profile.index');
+        $user = auth()->user();
+        return view('profile.index', compact('user'));
     }
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -7,24 +7,27 @@
     </div>
     {{-- USER FORM --}}
     <div class="w-8/12 bg-white p-6 rounded-lg font-bold">
+        <div class="w-96 bg-gray-100 p-6 rounded-lg font-bold mb-5">
+            Information Form
+        </div>
         <form action="{{ route('profile') }}" method="post">
             @csrf
             <div class="flex justify-around flex-wrap ">
                 <div class="w-96 my-2">
-                    <label for="email" class="sr-only">Email</label>
-                    <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+                    <label for="fullName" class="sr-only">Name</label>
+                    <input type="text" name="fullName" id="fullName" placeholder="Your full name" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('fullName') border-red-500 @enderror" value="{{ $user->name }}">
 
-                    @error('email')
+                    @error('fullName')
                         <div class="text-red-500 mt-2 text-sm">
                             {{ $message }}
                         </div>
                     @enderror
                 </div>
                 <div class="w-96 my-2">
-                    <label for="email" class="sr-only">Email</label>
-                    <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+                    <label for="username" class="sr-only">Username</label>
+                    <input type="text" name="username" id="username" placeholder="Your username" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('username') border-red-500 @enderror" value="{{ $user->username }}">
 
-                    @error('email')
+                    @error('username')
                         <div class="text-red-500 mt-2 text-sm">
                             {{ $message }}
                         </div>
@@ -34,7 +37,7 @@
             <div class="flex justify-around flex-wrap ">
                 <div class="w-96 my-2">
                     <label for="email" class="sr-only">Email</label>
-                    <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+                    <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ $user->email }}">
 
                     @error('email')
                         <div class="text-red-500 mt-2 text-sm">
@@ -43,10 +46,10 @@
                     @enderror
                 </div>
                 <div class="w-96 my-2">
-                    <label for="email" class="sr-only">Email</label>
-                    <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+                    <label for="phone" class="sr-only">Phone</label>
+                    <input type="text" name="phone" id="phone" placeholder="Your phone" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('phone') border-red-500 @enderror" value="{{ old('phone') }}">
 
-                    @error('email')
+                    @error('phone')
                         <div class="text-red-500 mt-2 text-sm">
                             {{ $message }}
                         </div>
